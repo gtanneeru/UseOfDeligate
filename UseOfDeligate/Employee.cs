70f55e9567edceb95c565867c1d50d8ee2a64167
gtanneeru <gtanneeru@gmail.com>
2016-09-30 23:14:43 -0400

    Deligate function

---
+++ UseOfDeligate/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseOfDeligate
+{
+   delegate bool isPromoted(Employee empl);
+    class Employee
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public int experience { get; set; }
+        public int salary { get; set; }
+
+       
+
+        public void getPromoted(List<Employee> employeeList, isPromoted whenEligible)
+        {
+            foreach(Employee emp in employeeList)
+            {
+                if(whenEligible(emp))
+                {
+                    Console.WriteLine(emp.name + " is promoted");
+                }
+            }
+        }
+    }
+}
--- UseOfDeligate/Program.cs
+++ UseOfDeligate/Program.cs
@@ -10,6 +10,26 @@ namespace UseOfDeligate
     {
         static void Main(string[] args)
         {
+            Employee emp = new Employee();
+            List<Employee> employee = new List<Employee>();
+            employee.Add(new Employee() { ID = 101, name = "Mary", experience = 5, salary = 5000 });
+            employee.Add(new Employee() { ID = 102, name = "John", experience = 2, salary = 4000 });
+            employee.Add(new Employee() { ID = 103, name = "Philips", experience = 6, salary = 6000 });
+            employee.Add(new Employee() { ID = 104, name = "Mark", experience = 4, salary = 5000 });
+
+
+             isPromoted isPromotable = new isPromoted(promote);
+             emp.getPromoted(employee,isPromotable);
+            // Instead of creating a bool function and an instance, we can use lambda expression, mainly used for deligates and linq coding.
+
+           // emp.getPromoted(employee, emplo => emp.experience >= 5);
+
+            Console.ReadKey();
+        }
+
+        public static bool promote(Employee empl)
+        {
+            return (empl.experience >= 5) ? true : false;
         }
     }
 }
